c23132e4a23ae2753ab8384b913d057cbd08e0aa
Luu Viet Bach <compbro05122007@gmail.com>
2024-07-09 01:59:55 +0700

    Add features to receive message content, test if it contains website name and if it comes from the doujin channels

--- doujin_manager.py
+++ doujin_manager.py
@@ -5,11 +5,28 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD = os.getenv('DISCORD_GUILD')
+CATEGORY = os.getenv("DISCORD_CATEGORY")
+DOUJIN_CHANNEL = os.getenv("DISCORD_DOUJIN_CHANNEL")
 
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.default()
+intents.message_content = True
+intents.messages = True
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
 	print(f'{client.user} has connected to Discord!')
+	global guild, category
+	guild = discord.utils.get(client.guilds, name=GUILD)
+	category = discord.utils.get(guild.categories, name=CATEGORY)
+
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+	if 'nhentai.net' in message.content and (message.channel.category is category or message.channel.name == DOUJIN_CHANNEL):
+		print(1)
 
 client.run(TOKEN)

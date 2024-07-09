ef7d49b657caa945a69de9858fb5f1beb1d7e759
Luu Viet Bach <compbro05122007@gmail.com>
2024-07-09 11:25:44 +0700

    Chane Client to Bot, add command prefix

--- doujin_manager.py
+++ doujin_manager.py
@@ -1,6 +1,7 @@
 import os
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 import sys
 
@@ -19,25 +20,23 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.messages = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(intents=intents, command_prefix="!")
 
-@client.event
+@bot.event
 async def on_ready():
-	print(f'{client.user} has connected to Discord!')
+	print(f'{bot.user} has connected to Discord!')
 	global guild, category
-	guild = discord.utils.get(client.guilds, name=GUILD)
+	guild = discord.utils.get(bot.guilds, name=GUILD)
 	category = discord.utils.get(guild.categories, name=CATEGORY)
 
-@client.event
+@bot.event
 async def on_message(message):
-	if message.author == client.user:
+	if message.author == bot.user:
 		return
 	if 'nhentai.net' in message.content and (message.channel.category is category or message.channel.name == DOUJIN_CHANNEL):
 		print(1)
-	elif message.content.isdecimal() and int(message.content) > 600000:
-		raise InvalidNuclearCode(int(message.content))
 
-@client.event
+@bot.event
 async def on_error(event, *args, **kwargs):
 	if event == 'on_message':
 		try:
@@ -45,4 +44,4 @@ async def on_error(event, *args, **kwargs):
 		except InvalidNuclearCode as e:
 			print(e.code)
 
-client.run(TOKEN)
+bot.run(TOKEN)
